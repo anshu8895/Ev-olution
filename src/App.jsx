f068b2f207fb7019d7c82ffa7ea7ec56a24987c7
Anshuman Padhi <136254225+anshu8895@users.noreply.github.com>
2024-01-06 19:08:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
     { text1: "Give in to", text2: "your desires" },
   ]
   const [heroCount, setHeroCount] = useState(0)
-  const [playStatus, setPlayStatus] = useState(false)
+  const [playStatus, setPlayStatus] = useState(true)
 
   useEffect(() => {
     const intervalId = setInterval(() => {
